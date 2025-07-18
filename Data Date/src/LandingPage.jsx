b27b840c759e7dbc9aaf43f7598a637369d2369c
minugem <closehaan@gmail.com>
2025-07-19 00:54:16 +1200

    getStarted Button

--- Data Date/src/LandingPage.jsx
+++ Data Date/src/LandingPage.jsx
@@ -8,15 +8,21 @@ function App() {
     <>
       <div className="nav">
         <div className='leftNav'>
-        <div>
-          <img src={Logo} alt="Logo" className="logo" />
-        </div>
-        <a href="" className="about">About</a>
+          <div>
+            <img src={Logo} alt="Logo" className="logo" />
+          </div>
+          <a href="" className="about">About</a>
         </div>
         <button className="login">Login</button>
       </div>
       <div className="hero">
-        
+        <div className="header">
+          <h1>Find your perfect match through <span className='gradient'>music</span>.</h1>
+          <div className="getStarted">
+            <button><span class="buttonText">Get Started</span></button>
+          </div>
+        </div>
+
       </div>
     </>
   )
