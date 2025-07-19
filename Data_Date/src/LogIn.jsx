511031e23dac122d577897bb3c32f18c15002de5
Ashwin Singh <162235137+nisa397@users.noreply.github.com>
2025-07-19 19:32:29 +1200

    Fixed button issue on landing page

--- Data_Date/src/LogIn.jsx
+++ Data_Date/src/LogIn.jsx
@@ -1,24 +1,21 @@
-import './styles/logIn.css'
+import styleLogin from './styles/logIn.module.css'
 import Logo from './assets/Logo.png'
 
 function LogIn() {
     return (
     <>
 
-    <div className='modal'>
 
-        <div className='modalContent'>
-        <img src={Logo} alt="Logo" className="logo" />
-        
-        <div className='LogInContainer'>
-            <input type="text" placeholder='Username' className='usernameInput' />
-            <input type="password" placeholder='Password' className='passwordInput' />
-            <button className='logInButton'>Log In</button>
-            <p className='registerText'>Don't have an account? <a href="/signup" className='registerLink'>Sign Up</a></p>
+    <div className={styleLogin.modal}>
+        <div className={styleLogin.modalContent}>
+            <img src={Logo} alt="Logo" className={styleLogin.logo} />
+            <div className={styleLogin.logInContainer}>
+                <input type="text" placeholder='Username' className={styleLogin.usernameInput} />
+                <input type="password" placeholder='Password' className={styleLogin.passwordInput} />
+                <button className={styleLogin.logInButton}>Log In</button>
+                <p className={styleLogin.registerText}>Don't have an account? <a href="/signup" className={styleLogin.registerLink}>Sign Up</a></p>
+            </div>
         </div>
-
-    </div>
-
     </div>
 
     
