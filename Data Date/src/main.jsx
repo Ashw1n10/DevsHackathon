9523451fd111f8fae6cd6676c2e8c3238c38286f
Calvin Nair <cnai408@aucklanduni.ac.nz>
2025-07-19 17:59:19 +1200

    created user profile page

--- Data Date/src/main.jsx
+++ Data Date/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import LandingPage from './LandingPage.jsx'
+import UserProfile from './UserProfile.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <LandingPage />
+    //  <LandingPage />
+    <UserProfile />
 )
