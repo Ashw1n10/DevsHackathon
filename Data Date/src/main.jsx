5073d07271881215eab32e321967b036bd38e46a
minugem <closehaan@gmail.com>
2025-07-19 12:38:11 +1200

    Created Matches Page

--- Data Date/src/main.jsx
+++ Data Date/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import LandingPage from './LandingPage.jsx'
+import MatchesPage from './MatchesPage.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <LandingPage />
+    // <LandingPage />
+    <MatchesPage />
 )
