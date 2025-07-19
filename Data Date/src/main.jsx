94f6610b073da332abf4a3f3167f2fa619bb1672
Ashwin Singh <162235137+nisa397@users.noreply.github.com>
2025-07-19 16:56:23 +1200

    Created log in

--- Data Date/src/main.jsx
+++ Data Date/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import LandingPage from './LandingPage.jsx'
-
+import LogIn from './LogIn.jsx'
 createRoot(document.getElementById('root')).render(
-    <LandingPage />
+    <LogIn />
 )
