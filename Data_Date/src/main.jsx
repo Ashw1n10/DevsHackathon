6e269ffe45ad844dfb99e3a1f76093ff02e8070f
Nico Abrasado <nabr105@aucklanduni.ac.nz>
2025-07-19 19:03:12 +1200

    Merge conflict fixing attempt

--- Data_Date/src/main.jsx
+++ Data_Date/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/index.css'
+import LandingPage from './LandingPage.jsx'
+import SignUp from './SignUp.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<LandingPage />
 )
