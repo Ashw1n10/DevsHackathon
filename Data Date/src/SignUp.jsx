bd47939dace270c81a89e74be935d440a56c3616
Ashwin Singh <162235137+nisa397@users.noreply.github.com>
2025-07-19 18:02:40 +1200

    Added sign up page

---
+++ Data Date/src/SignUp.jsx
@@ -0,0 +1,58 @@
+import './styles/signUp.css'
+import Logo from './assets/Logo.png'
+
+function SignUp() {
+    return (
+        <>
+
+
+
+            <div className='modal'>
+                <div className='modalContent'>
+
+                    <img src={Logo} alt="Logo" className="logo" />
+
+                    <div className='SignUpContainer'>
+
+                        <input type="text" placeholder='Enter your Username' className='usernameInput' />
+                        <input type="password" placeholder='Enter your Password' className='passwordInput' />
+                        <input type="password" placeholder='Confirm Password' className='confirmPasswordInput' />
+                        <input type="email" placeholder='Email' className='emailInput' />
+                        <input type="text" placeholder='Full Name' className='nameInput' />
+                        <label for="gender">Select your gender</label>
+                        <select name="gender" id="gender">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="Non-binary">Non-binary</option>
+                        </select>
+                        <label for="age">Select your age</label>
+                        <input type="number" id="age" name="age" min="18" max="100" placeholder='Age' className='ageInput' />
+                        <label for="Preference">Select your preference</label>
+                        <select name="Preference" id="Preference">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="Non-binary">Non-binary</option>
+                            <option value="any">Any</option>
+                        </select>
+
+                        <button>Link Spotify</button>
+
+                        <button className='Sign Up'>Sign Up</button>
+                    </div>
+
+
+
+
+
+
+                </div>
+
+            </div>
+
+
+
+
+        </>)
+}
+
+export default SignUp
